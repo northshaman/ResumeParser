0f2873f4218a887f9a85d21d56c1b5ad2f9aeffa
northshaman <north-shaman@mail.ru>
2018-07-13 11:24:48 +0500

    add RootConfig class and WebDriver Bean also add dependency for Selenium and PhantomJS add profiles in Maven extract properties for parser tools to the top of POM

--- src/main/java/com/shaman/parser/config/RootConfig.java
+++ src/main/java/com/shaman/parser/config/RootConfig.java
@@ -1,8 +1,31 @@
 package com.shaman.parser.config;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
 /**
  *
  */
+@Configuration
+@PropertySource({"classpath:config.properties","classpath:css.properties"})
+@ComponentScan({"com.shaman.parser"})
 public class RootConfig {
+
+    @Autowired
+    private
+    Environment env;
+
+    @Bean(destroyMethod = "quit")
+    public WebDriver getPhantomJSWebDriver(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, env.getRequiredProperty("phantomJS.path"));
+        return new PhantomJSDriver(caps);
+    }
 
 }
